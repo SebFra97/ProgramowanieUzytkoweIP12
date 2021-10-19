6a4a39d63a231399de6e1264c88042393feb7cb3
SebFra97 <sebastian.krzysztof.franc@gmail.com>
2021-10-19 09:51:39 +0200

    Dodanie kontrollera RepositoryPattern. Czyszczenie.

---
+++ ProgramowanieUzytkoweIP12/Controllers/RepositoryPatternController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+using RepositoryPattern;
+using System;
+using System.Collections.Generic;
+
+namespace ProgramowanieUzytkoweIP12.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepositoryPatternController : ControllerBase
+    {
+        private IBookRepository _bookRepository;
+        private IAuthorRepository _authorRepository;
+
+        public RepositoryPatternController(IBookRepository bookRepository, IAuthorRepository authorRepository)
+        {
+            _bookRepository = bookRepository;
+            _authorRepository = authorRepository;
+        }
+
+        #region BOOK ENDPOINTS
+
+        [HttpGet("/book/get")]
+        public List<BookDto> GetBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        [HttpGet("/book/get/{id}")]
+        public BookDto GetBook([FromRoute] int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        [HttpPost("/book/create")]
+        public ActionResult<Book> CreateBook(Book book)
+        {
+            try
+            {
+                _bookRepository.CreateNewBook(book);
+                return Ok(book);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("/book/add/rate/{id}")]
+        public ActionResult AddRateToBook([FromRoute] int id, int rate)
+        {
+            try
+            {
+                _bookRepository.AddRateToBook(id, rate);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("/book/delete/{id}")]
+        public ActionResult DeleteBook([FromRoute] int id)
+        {
+            try
+            {
+                _bookRepository.DeleteBook(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        #endregion
+
+        #region AUTHOR ENDPOINTS
+
+        [HttpGet("/authors/get")]
+        public List<BookDto> GetAuthor()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        [HttpPost("/author/create")]
+        public ActionResult<Book> CreateAuthor(Author author)
+        {
+            try
+            {
+                _authorRepository.CreateNewAuthor(author);
+                return Ok(author);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("/author/add/rate/{id}")]
+        public ActionResult AddRateToAuthor([FromRoute] int id, int rate)
+        {
+            try
+            {
+                _authorRepository.AddRateToAuthor(id, rate);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("/author/delete/{id}")]
+        public ActionResult DeleteAuthor([FromRoute] int id)
+        {
+            try
+            {
+                _authorRepository.DeleteAuthor(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        #endregion
+
+    }
+}
