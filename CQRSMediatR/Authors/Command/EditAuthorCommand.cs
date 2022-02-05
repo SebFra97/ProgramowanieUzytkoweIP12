1283ace6861c0fd4188833340138f50120f44e4a
SebFra97 <sebastian.krzysztof.franc@gmail.com>
2022-02-05 21:50:41 +0100

    wip modale edycyjne + dodania oceny

---
+++ CQRSMediatR/Authors/Command/EditAuthorCommand.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using Model;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRSMediatR.Authors.Command
+{
+    public class EditAuthorCommand : IRequest<bool>
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string CV { get; set; }
+
+        public class EditAuthorCommandHandler : IRequestHandler<EditAuthorCommand, bool>
+        {
+            private ApplicationDbContext context;
+
+            public EditAuthorCommandHandler(ApplicationDbContext context)
+            {
+                this.context = context;
+            }
+
+            public Task<bool> Handle(EditAuthorCommand request, CancellationToken cancellationToken)
+            {
+                var findAuthor = context.Authors.FirstOrDefault(x => x.Id == request.AuthorId);
+
+                if (findAuthor == null) return Task.FromResult(false);
+
+                if (findAuthor.FirstName != request.Name)
+                {
+                    findAuthor.FirstName = request.Name;
+                }
+
+                if (findAuthor.SecondName != request.SurName)
+                {
+                    findAuthor.SecondName = request.SurName;
+                }
+
+                if (findAuthor.CV != request.CV)
+                {
+                    findAuthor.CV = request.CV;
+                }
+
+                context.SaveChanges();
+
+                return Task.FromResult(true);
+
+            }
+        }
+    }
+}
---
+++ CQRSMediatR/Books/Command/EditBookCommand.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Model;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRSMediatR.Books.Command
+{
+    public class EditBookCommand : IRequest<bool>
+    {
+        public int BookId { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+
+        public class EditBookCommandHandler : IRequestHandler<EditBookCommand, bool>
+        {
+            private ApplicationDbContext context;
+
+            public EditBookCommandHandler(ApplicationDbContext context)
+            {
+                this.context = context;
+            }
+
+            public Task<bool> Handle(EditBookCommand request, CancellationToken cancellationToken)
+            {
+                var findBook = context.Books.FirstOrDefault(x => x.Id == request.BookId);
+
+                if (findBook == null) return Task.FromResult(false);
+
+                if(findBook.Description != request.Description)
+                {
+                    findBook.Description = request.Description;
+                }
+
+                if (findBook.Title != request.Title)
+                {
+                    findBook.Title = request.Title;
+                }
+
+                if (findBook.ReleaseDate != request.ReleaseDate)
+                {
+                    findBook.ReleaseDate = request.ReleaseDate;
+                }
+
+                context.SaveChanges();
+
+                return Task.FromResult(true);
+
+            }
+        }
+    }
+}
